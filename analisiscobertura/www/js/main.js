45a524bc9596fe6afeeadb56bf665ef3b1ae14d1
innovatelecom <59919549+innovatelecom@users.noreply.github.com>
2022-05-26 18:50:52 +0200

    Corregidas no conformidades accesibilidad
    Corregidas no conformidades accesibilidad.

--- analisiscobertura/www/js/main.js
+++ analisiscobertura/www/js/main.js
@@ -14,17 +14,17 @@ var MAIN = (function() {
 
     ret.clickEvent = ($.support.touch ? "tap" : "click");
 
-    ret.localizacionParaDebug = false; //FIXME: Poner a false cuando se generen versiones para Aragón.
+    ret.localizacionParaDebug = true; //FIXME: Poner a false cuando se generen versiones para Aragón.
     ret.esVersionWeb = false; //FIXME: Poner a true si generamos la versión Browser. Poner a false si generamos la versión de la App para Android o iOS.
 
-    //ret.entorno = "DEV";
-    //ret.urlWS = "https://wsdevcobertura.itsoft.es/api";
+    ret.entorno = "DEV";
+    ret.urlWS = "https://wsdevcobertura.itsoft.es/api";
 
     //ret.entorno = "DEV_ARAGON";
     //ret.urlWS = "https://desopendataei2a.aragon.es/cobertura/api/api";
 
-    ret.entorno = "PRE_ARAGON";
-    ret.urlWS = "https://preopendataei2a.aragon.es/cobertura/api/api";
+    //ret.entorno = "PRE_ARAGON";
+    //ret.urlWS = "https://preopendataei2a.aragon.es/cobertura/api/api";
 
     //ret.entorno = "PROD_ARAGON";
     //ret.urlWS = "https://miv-aodei2a-01.aragon.local:4000/ws-cobertura/api";
